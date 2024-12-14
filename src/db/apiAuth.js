4450bbdf7ab95959d71880e630287c03ca893b99
Anish Pillai <anishp8914@gmail.com>
2024-12-14 19:53:33 +0530

    sign up api added

--- src/db/apiAuth.js
+++ src/db/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function login({email, password}) {
     const {data, error} = await supabase.auth.signInWithPassword({
@@ -21,4 +21,30 @@ export async function getCurrentUser() {
 
   if (error) throw new Error(error.message);
   return session.session?.user;
+}
+
+//Function to sign up new user
+export async function signUp({name,email,password,profile_pic}){
+
+  //upload profile pic
+  const filename=`dp-${name.split(" ").join("-")}-${Math.random()}`;
+  const {error:storageError}=await supabase.storage.from("profile_pic").upload(filename,profile_pic);
+
+  if(storageError) throw new Error(storageError.message);
+
+  //sign up
+  const {data,error}=await supabase.signUp({
+    email,
+    password,
+    options:{
+      data:{
+        name,
+        profile_pic:`${supabaseUrl}/storage/v1/object/public/profile_pic/${filename}`
+      }
+    }
+  });
+
+  if(error) throw new Error(error.message);
+
+  return data;
 }
